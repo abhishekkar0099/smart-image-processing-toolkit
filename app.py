6e1dbed56ee5fd41a9f30a03af711d97f97813a1
akash09999999 <ytakash308@gmail.com>
2026-04-30 14:14:38 +0530

    ready for deployment

--- app.py
+++ app.py
@@ -133,4 +133,6 @@ def stop_camera():
     return ("", 204)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
